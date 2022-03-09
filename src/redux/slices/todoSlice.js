55f8d510589e749a0aca3678fdb06e865413718c
MAbdurahman <mdbdrrhm@gmail.com>
2022-03-09 04:13:11 -0600

    Add redux functionality to addTodo reducer and action

---
+++ src/redux/slices/todoSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const getInitialTodos = () => {
+	//****************** get todoList ******************//
+	const localTodoList = window.localStorage.getItem('todoList');
+
+	//*** parse todoList to json format if not empty ***//
+	if (localTodoList) {
+		return JSON.parse(localTodoList);
+	}
+	window.localStorage.setItem('todoList', []);
+	return [];
+};
+
+const initialValue = {
+	todoList: getInitialTodos(),
+};
+
+export const todoSlice = createSlice({
+	name: todo,
+	initialState: initialValue,
+	reducers: {
+		addTodo: (state, action) => {
+			state.todoList.push(action.payload);
+			const todoList = window.localStorage.getItem('todoList');
+			if (todoList) {
+				const todoListArr = JSON.parse(todoList);
+				todoListArr.push({
+					...action.payload,
+				});
+				window.localStorage.setItem(
+					'todoList',
+					JSON.stringify(todoListArr)
+				);
+			} else {
+				window.localStorage.setItem(
+					'todoList',
+					JSON.stringify([
+						{
+							...action.payload,
+						},
+					])
+				);
+			}
+		},
+	},
+});

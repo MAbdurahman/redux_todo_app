ef375fffa4342152266695b3fa5e92a30d5b1a3c
MAbdurahman <mdbdrrhm@gmail.com>
2022-03-08 18:19:52 -0600

    Add Button and SelectButton

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PageTitle from './../components/PageTitle';
+import AppHeader from './../components/AppHeader';
 import styles from './../styles/modules/app.module.scss';
 
 export default function App() {
@@ -8,7 +9,7 @@ export default function App() {
 			<div className='container'>
 				<PageTitle>ToDo List</PageTitle>
 				<div className={styles.app__wrapper}>
-					
+					<AppHeader />
 				</div>
 			</div>
 		</>
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button, { SelectButton } from './Button';
+import styles from './../styles/modules/app.module.scss';
+
+export default function AppHeader() {
+	return (
+		<div className={styles.appHeader}>
+			<Button variant='primary'>Add Task</Button>
+			<SelectButton>
+				<option value='all'>All</option>
+				<option value='incomplete'>Incomplete</option>
+				<option value='complete'>Completed</option>
+			</SelectButton>
+		</div>
+	);
+}
